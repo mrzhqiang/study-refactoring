5481dc5e80a1e653725dbb20d0bd52fcbeb79708
mrzhqiang <287431404@qq.com>
2019-10-21 22:58:54 +0800

    更新：1.1 起点

---
+++ src/chapter01/Customer.java
@@ -0,0 +1,64 @@
+package chapter01;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+class Customer {
+    private String name;
+    private Vector<Rental> rentals = new Vector<>();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addRental(Rental rental) {
+        this.rentals.addElement(rental);
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoint = 0;
+        Enumeration<Rental> rentals = this.rentals.elements();
+        String result = "Rental record for " + getName() + "\n";
+        while (rentals.hasMoreElements()) {
+            double thisAmount = 0;
+            Rental each = rentals.nextElement();
+
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2) {
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3) {
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
+                    break;
+            }
+
+            frequentRenterPoint ++;
+
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                    each.getDaysRented() > 1) {
+                frequentRenterPoint++;
+            }
+
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+            totalAmount += thisAmount;
+        }
+
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoint) + " frequent renter points";
+        return result;
+    }
+}
---
+++ src/chapter01/Main.java
@@ -0,0 +1,8 @@
+package chapter01;
+
+public class Main {
+    public static void main(String[] args) {
+        Customer customer = new Customer("test 1");
+        System.out.println(customer.statement());
+    }
+}
