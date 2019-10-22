e9658dd4378dbcfefae595b3177160d9a3501a5c
mrzhqiang <287431404@qq.com>
2019-10-22 11:32:19 +0800

    更新：1.3.3 去除临时变量

--- src/main/java/chapter01/Customer.java
+++ src/main/java/chapter01/Customer.java
@@ -20,21 +20,36 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoint = 0;
         Enumeration<Rental> rentals = this.rentals.elements();
         String result = "Rental record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = rentals.nextElement();
 
-            frequentRenterPoint += each.getFrequentRenterPoints();
-
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
 
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoint) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration<Rental> rentals = this.rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental rental = rentals.nextElement();
+            result += rental.getFrequentRenterPoints();
+        }
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration<Rental> rentals = this.rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental rental = rentals.nextElement();
+            result += rental.getCharge();
+        }
         return result;
     }
 }
